1e0801ed483f80f99846088d997fc28a8cb08f1c
usuario <usuario@10.22.143.31>
2015-10-14 09:55:50 +0100

    Primera versión

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -1,40 +1,40 @@
-
 package javaapplication1;
-
-
 
 public class JavaApplication1 {
 
 
     public static void main(String[] args) {
+       
+        
        Histogram<String> histogram = new Histogram<>();
-       histograma.increment("gmail.com");
-       histograma.increment("gmail.com");
-       histograma.increment("gmail.com");
-       histograma.increment("gmail.com");
-       histograma.increment("gmail.com");
+       histogram.increment("gmail.com");
+       histogram.increment("gmail.com");
+       histogram.increment("gmail.com");
+       histogram.increment("gmail.com");
+       histogram.increment("gmail.com");
        
-       histograma.increment("hotmail.com");
-       histograma.increment("hotmail.com");
-       histograma.increment("hotmail.com");
-       histograma.increment("hotmail.com");
-       histograma.increment("hotmail.com");
+       histogram.increment("hotmail.com");
+       histogram.increment("hotmail.com");
+       histogram.increment("hotmail.com");
+       histogram.increment("hotmail.com");
+       histogram.increment("hotmail.com");
        
-       histograma.increment("ulpgc.es");
-       histograma.increment("ulpgc.es");
-       histograma.increment("ulpgc.es");
-       histograma.increment("ulpgc.es");
-       histograma.increment("ulpgc.es");
-       histograma.increment("ulpgc.es");
-       histograma.increment("ulpgc.es");
-       histograma.increment("ulpgc.es");
-       histograma.increment("ulpgc.es");
-       histograma.increment("ulpgc.es");
-       histograma.increment("ulpgc.es");
-       histograma.increment("ulpgc.es");
-       histograma.increment("ulpgc.es");
+       histogram.increment("ulpgc.es");
+       histogram.increment("ulpgc.es");
+       histogram.increment("ulpgc.es");
+       histogram.increment("ulpgc.es");
+       histogram.increment("ulpgc.es");
+       histogram.increment("ulpgc.es");
+       histogram.increment("ulpgc.es");
+       histogram.increment("ulpgc.es");
+       histogram.increment("ulpgc.es");
+       histogram.increment("ulpgc.es");
+       histogram.increment("ulpgc.es");
+       histogram.increment("ulpgc.es");
+       histogram.increment("ulpgc.es");
        
-       HistogramDisplay histo = new HistogramaDisplay(histograma);
+       HistogramaDisplay histo;
+        histo = new HistogramaDisplay(histogram);
         histo.execute();
     }
 }
