b379a3b2598e9c50879b3c1fe47907ce23f83cbb
usuario <usuario@10.22.143.31>
2015-10-14 10:36:42 +0100

    Cuarta versión

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -10,21 +10,18 @@ public class JavaApplication1 {
     public static void main(String[] args) {
        
         
-       Histogram<String> histogram = new Histogram<>();
        String pathName = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\JavaApplication1\\Data\\emails.txt";
        MailReader dominios = new MailReader(pathName);
        ArrayList <String> domis =dominios.getDominiosArray();
-        for (int i = 0; i < domis.size(); i++) {
-            histogram.increment(domis.get(i));
-            
-        }
+       
+       Histogram <String> histograma = DominiosHistogramBuilder.execute(domis);
  
         
 
        
 
        HistogramaDisplay histo;
-        histo = new HistogramaDisplay(histogram);
+        histo = new HistogramaDisplay(histograma);
         histo.execute();
     }
 }
